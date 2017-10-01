9ef2e8c92027b3c21ab67110dc5b2d5f1be9f7da
EXcurrybat <32018466+EXcurrybat@users.noreply.github.com>
2017-10-01 01:40:37 -0700

    aaaaaaaaaa

--- Assignment 1/myjs.js
+++ Assignment 1/myjs.js
@@ -9,27 +9,31 @@ var quaright = 100;
 var img = document.getElementById('img1');
 var imgW = img.clientWidth;
 var imgH = img.clientHeight;
-var imgtop = onetop;
-var imgleft = oneleft;
-var imgbot = tribot;
-var imgright = quaright;
 
 document.getElementById("reset").addEventListener("click", function () {
-    if (img == document.getElementById("img1")) {
+    if (img === document.getElementById("img1")) {
         img.style.left = "0px";
         img.style.top = "100px";
+        onetop = 100;
+        oneleft = 0;
     }
-    if (img == document.getElementById("img2")) {
+    if (img === document.getElementById("img2")) {
         img.style.left = "100px";
         img.style.top = "0px";
+        twotop = 0;
+        twoleft = 100;
     }
-    if (img == document.getElementById("img3")) {
+    if (img === document.getElementById("img3")) {
+        img.style.bottom = "100px";
         img.style.left = "0px";
-        img.style.bot = "100px";
+        tribot = 100;
+        trileft = 0;
     }
-    if (img == document.getElementById("img4")) {
+    if (img === document.getElementById("img4")) {
         img.style.right = "100px";
         img.style.top = "0px";
+        quatop = 0;
+        quaright = 100;
     }
     img.style.height = "200px";
     img.style.width = "auto";
@@ -46,91 +50,123 @@ document.getElementById("close").addEventListener("click", function () {
 });
 
 document.getElementById("imgA").addEventListener("click", function () {
-    if (img == document.getElementById('img1')) {
+    if (img === document.getElementById('img1')) {
         img.src = "1.jpg";
     }
-    if (img == document.getElementById('img2')) {
+    if (img === document.getElementById('img2')) {
         img.src = "4.jpg";
     }
-    if (img == document.getElementById('img3')) {
+    if (img === document.getElementById('img3')) {
         img.src = "7.jpg";
     }
-    if (img == document.getElementById('img4')) {
+    if (img === document.getElementById('img4')) {
         img.src = "10.jpg";
     }
 });
 
 document.getElementById("imgB").addEventListener("click", function () {
-    if (img == document.getElementById('img1')) {
+    if (img === document.getElementById('img1')) {
         img.src = "2.jpg";
     }
-    if (img == document.getElementById('img2')) {
+    if (img === document.getElementById('img2')) {
         img.src = "5.jpg";
     }
-    if (img == document.getElementById('img3')) {
+    if (img === document.getElementById('img3')) {
         img.src = "8.jpg";
     }
-    if (img == document.getElementById('img4')) {
+    if (img === document.getElementById('img4')) {
         img.src = "11.jpg";
     }
 });
 
 document.getElementById("imgC").addEventListener("click", function () {
-    if (img == document.getElementById('img1')) {
+    if (img === document.getElementById('img1')) {
         img.src = "3.jpg";
     }
-    if (img == document.getElementById('img2')) {
+    if (img === document.getElementById('img2')) {
         img.src = "6.jpg";
     }
-    if (img == document.getElementById('img3')) {
+    if (img === document.getElementById('img3')) {
         img.src = "9.jpg";
     }
-    if (img == document.getElementById('img4')) {
+    if (img === document.getElementById('img4')) {
         img.src = "12.jpg";
     }
 });
 
 document.getElementById("left").addEventListener("click", function () {
-    if (img != document.getElementById('img4')) {
-        imgleft=imgleft-5;
-        img.style.left = imgleft+"px";
+    if (img === document.getElementById('img1')) {
+        oneleft=oneleft-5;
+        img.style.left = oneleft+"px";
     }
-    else {
-        imgright=imgright+5;
-        img.style.right = imgright+"px";
+    if (img === document.getElementById('img2')) {
+        twoleft=twoleft-5;
+        img.style.left = twoleft+"px";
+    }
+    if (img === document.getElementById('img3')) {
+        trileft=trileft-5;
+        img.style.left = trileft+"px";
+    }
+    if (img === document.getElementById('img4')) {
+        quaright=quaright+5;
+        img.style.right = quaright+"px";
     }
 });
 
 document.getElementById("right").addEventListener("click", function () {
-    if (img != document.getElementById('img4')) {
-        imgleft=imgleft+5;
-        img.style.left = imgleft+"px";
+    if (img === document.getElementById('img1')) {
+        oneleft=oneleft+5;
+        img.style.left = oneleft+"px";
     }
-    else {
-        imgright=imgright-5;
-        img.style.right = imgright+"px";
+    if (img === document.getElementById('img2')) {
+        twoleft=twoleft+5;
+        img.style.left = twoleft+"px";
+    }
+    if (img === document.getElementById('img3')) {
+        trileft=trileft+5;
+        img.style.left = trileft+"px";
+    }
+    if (img === document.getElementById('img4')) {
+        quaright=quaright-5;
+        img.style.right = quaright+"px";
     }
 });
 
 document.getElementById("up").addEventListener("click", function () {
-    if (img != document.getElementById("img3")) {
-        imgtop=imgtop-5;
-        img.style.top = imgtop+"px";
+    if (img === document.getElementById('img1')) {
+        onetop=onetop-5;
+        img.style.top = onetop+"px";
     }
-    else {
-        imgbot=imgbot+5;
-        img.style.bottom = imgbot+"px";
+    if (img === document.getElementById('img2')) {
+        twotop=twotop-5;
+        img.style.top = twotop+"px";
+    }
+    if (img === document.getElementById('img4')) {
+        quatop=quatop-5;
+        img.style.top = quatop+"px";
+    }
+    if (img === document.getElementById('img3')) {
+        tribot=tribot+5;
+        img.style.bottom = tribot+"px";
     }
 });
 
 document.getElementById("down").addEventListener("click", function () {
-    if (img != document.getElementById("img3")) {
-        imgtop=imgtop+5;
-        img.style.top = imgtop+"px";
+    if (img === document.getElementById('img1')) {
+        onetop=onetop+5;
+        img.style.top = onetop+"px";
     }
-    else {
-        imgbot=imgbot-5;
-        img.style.bottom = imgbot+"px";
+    if (img === document.getElementById('img2')) {
+        twotop=twotop+5;
+        img.style.top = twotop+"px";
+    }
+    if (img === document.getElementById('img4')) {
+        quatop=quatop+5;
+        img.style.top = quatop+"px";
+    }
+    if (img === document.getElementById('img3')) {
+        tribot=tribot-5;
+        img.style.bottom = tribot+"px";
     }
 });
 
@@ -152,8 +188,6 @@ document.getElementById("img1").addEventListener("click", function(){
     img = document.getElementById("img1");
     imgW = img.clientWidth;
     imgH = img.clientHeight;
-    imgtop = onetop;
-    imgleft = oneleft;
     document.getElementById("background").style.backgroundImage = "url( bg1.jpg)";
     document.getElementById("imgA").src = "1.jpg";
     document.getElementById("imgB").src = "2.jpg";
@@ -164,8 +198,6 @@ document.getElementById("img2").addEventListener("click", function(){
     img = document.getElementById("img2");
     imgW = img.clientWidth;
     imgH = img.clientHeight;
-    imgtop = twotop;
-    imgleft = twoleft;
     document.getElementById("background").style.backgroundImage = "url( bg2.jpg)";
     document.getElementById("imgA").src = "4.jpg";
     document.getElementById("imgB").src = "5.jpg";
@@ -176,8 +208,6 @@ document.getElementById("img3").addEventListener("click", function(){
     img = document.getElementById("img3");
     imgW = img.clientWidth;
     imgH = img.clientHeight;
-    imgbot = tribot;
-    imgleft = trileft;
     document.getElementById("background").style.backgroundImage = "url( bg3.jpg)";
     document.getElementById("imgA").src = "7.jpg";
     document.getElementById("imgB").src = "8.jpg";
@@ -188,8 +218,6 @@ document.getElementById("img4").addEventListener("click", function(){
     img = document.getElementById("img4");
     imgW = img.clientWidth;
     imgH = img.clientHeight;
-    imgtop = quatop;
-    imgright = quaright;
     document.getElementById("background").style.backgroundImage = "url( bg4.jpg)";
     document.getElementById("imgA").src = "10.jpg";
     document.getElementById("imgB").src = "11.jpg";
